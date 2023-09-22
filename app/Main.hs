5d327b3961620e3d4445db502cb492edc7b3303f
Kevin Kenyon <kvkenyon@gmail.com>
2023-09-21 22:14:03 -0500

    Add basic parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,28 @@
 module Main where
 
-import System.Environment
+import System.Environment (getArgs)
 import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right _ -> "Found value"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
